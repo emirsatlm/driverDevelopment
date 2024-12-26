57ce72cd1ec7d1510653ba079b3492c7b71c3d41
emirsatlm <emirsatlm@gmail.com>
2024-12-26 21:36:36 +0100

    driverDevelopment

---
+++ Debug/Startup/startup_stm32h503rbtx.d
@@ -0,0 +1,1 @@
+Startup/startup_stm32h503rbtx.o: ../Startup/startup_stm32h503rbtx.s
---
+++ myDrivers/Inc/stm32h503xx.h
@@ -0,0 +1,13 @@
+/*
+ * stm32h503xx.h
+ *
+ *  Created on: Dec 26, 2024
+ *      Author: Emir
+ */
+
+#ifndef INC_STM32H503XX_H_
+#define INC_STM32H503XX_H_
+
+
+
+#endif /* INC_STM32H503XX_H_ */
